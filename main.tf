a26212ddf0b235e80dd7530140ebf977814fa785
Andrey Kryvor <akryvor@gmail.com>
2025-06-12 12:47:02 -0400

    typo

--- main.tf
+++ main.tf
@@ -36,13 +36,12 @@ module "web_vpc" {
 module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "8.3.0"
-  # insert the 1 required variable here
   name = "web"
   min_size = 1
   max_size = 2
 
   vpc_zone_identifier = module.web_vpc.public_subnets
-  target_group_arns   = module.web_elb.target_group_arns
+  target_group_arns   = module.web_alb.target_group_arns
   security_groups     = [module.web_sg.security_group_id]
 
   image_id      = data.aws_ami.app_ami.id
